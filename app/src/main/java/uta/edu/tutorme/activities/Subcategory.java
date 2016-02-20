a1b8fa4103454c214102c2b2dfcfd805936aba02
Hiral Trivedi <trivedihiralm@gmail.com>
2016-02-20 17:56:53 -0600

    Category and sub category activites added

---
+++ app/src/main/java/uta/edu/tutorme/activities/Subcategory.java
@@ -0,0 +1,16 @@
+package uta.edu.tutorme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uta.edu.tutorme.R;
+
+public class Subcategory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subcategory);
+    }
+}
+
