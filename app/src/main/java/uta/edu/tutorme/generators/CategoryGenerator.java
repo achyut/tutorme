47e32b8ad29e69ca673064cc1e96b2345b080f10
Achyut Paudel <achyut.pdl@gmail.com>
2016-02-23 06:17:10 -0600

    Master record model and repository added

--- app/src/main/java/uta/edu/tutorme/generators/CategoryGenerator.java
+++ app/src/main/java/uta/edu/tutorme/generators/CategoryGenerator.java
@@ -3,13 +3,13 @@ package uta.edu.tutorme.generators;
 import uta.edu.tutorme.exceptions.RecordNotFoundException;
 import uta.edu.tutorme.models.Category;
 import uta.edu.tutorme.repositories.CategoryRepository;
+import uta.edu.tutorme.repositories.MasterRecordRepository;
 
 /**
  * Created by anmolb77 on 2/20/2016.
  */
 public class CategoryGenerator {
     CategoryRepository repository;
-
     public CategoryGenerator(CategoryRepository repository) {
         this.repository = repository;
     }
@@ -42,10 +42,17 @@ public class CategoryGenerator {
         } catch (RecordNotFoundException e) {
             e.printStackTrace();
         }
-        repository.save(1, category_1);
-        repository.save(2, category_2);
-        repository.save(3, category_3);
-        repository.save(4, category_4);
-        repository.save(5, category_5);
+        MasterRecordRepository masterRecordRepository = new MasterRecordRepository();
+        String populatecat = masterRecordRepository.getValue("populatecategory");
+        if(populatecat==null || populatecat.equalsIgnoreCase("false")){
+            repository.save(1, category_1);
+            repository.save(2, category_2);
+            repository.save(3, category_3);
+            repository.save(4, category_4);
+            repository.save(5, category_5);
+            masterRecordRepository.addRecrord("populatecategory","true");
+        }
+
+
     }
 }
