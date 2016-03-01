7fd1838b75f02a47d1dbf88220e8d9c6f7932418
Achyut Paudel <achyut.pdl@gmail.com>
2016-03-01 07:28:29 -0600

    New post and post detail activity created

---
+++ app/src/main/java/uta/edu/tutorme/activities/PostActivity.java
@@ -0,0 +1,15 @@
+package uta.edu.tutorme.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uta.edu.tutorme.R;
+
+public class PostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post);
+    }
+}
