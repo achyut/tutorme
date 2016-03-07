89bf4d9275b8ec03d022f5a59b70ce4ccfb03565
Achyut Paudel <achyut.pdl@gmail.com>
2016-03-07 08:33:35 -0600

    Toolbar code removed'

--- app/src/main/java/uta/edu/tutorme/activities/AddNewAdvActivity.java
+++ app/src/main/java/uta/edu/tutorme/activities/AddNewAdvActivity.java
@@ -25,11 +25,9 @@ public class AddNewAdvActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_adv);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.hide();
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,27 +36,6 @@ public class AddNewAdvActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_new_adv, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
     private void sendPost()
     {
         Intent data = new Intent();
