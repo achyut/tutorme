267f49d026cf01f51be6c569bf0b4ab8b693f479
Achyut Paudel <achyut.pdl@gmail.com>
2016-04-11 02:40:54 -0500

    post detail activity updated

--- app/src/main/java/uta/edu/tutorme/utils/Urls.java
+++ app/src/main/java/uta/edu/tutorme/utils/Urls.java
@@ -6,7 +6,7 @@ package uta.edu.tutorme.utils;
 public class Urls {
 
     //public static String BASE_URL = "http://192.168.43.245:8000";
-    public static String BASE_URL = "http://0ef3b184.ngrok.io";
+    public static String BASE_URL = "http://8de16870.ngrok.io";
 
     public static String LOGIN_URL= BASE_URL+"/login";
     public static String FORGOT_PASSWORD_URL = BASE_URL+"/forgot";
